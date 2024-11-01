cbbbe7c28652179c478366a034db59f750d87475
Minkyu Lee <whizkyu@snu.ac.kr>
2024-11-02 01:21:31 +0900

    assignment2: get_db_session 오류 수정

--- wapang/database/connection.py
+++ wapang/database/connection.py
@@ -1,4 +1,4 @@
-from typing import Generator
+from typing import Any, Generator
 from sqlalchemy import create_engine
 from sqlalchemy.orm import Session, sessionmaker
 
@@ -20,12 +20,13 @@ class DatabaseManager:
 # TODO 아래 함수를 이용해서 Session 종속성을 주입받을 수 있습니다.
 # 이렇게 했을 때의 장점은 무엇일까요? 다른 방법은 없을까요?
 # 한 번 생각해보세요.
-def get_db_session() -> Generator[Session]:
+def get_db_session() -> Generator[Session, Any, None]:
     session = DatabaseManager().session_factory()
     try:
         yield session
         session.commit()
-    except:
+    except Exception as e:
         session.rollback()
+        raise e
     finally:
         session.close()
