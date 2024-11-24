c7fa32061e0811a3868a4bd855660d1876fa2a89
Minkyu Lee <whizkyu@snu.ac.kr>
2024-11-25 01:55:44 +0900

    refactor: 스토어에서 서비스 로직 제거

--- wapang/app/store/service.py
+++ wapang/app/store/service.py
@@ -2,7 +2,11 @@ from typing import Annotated
 
 from fastapi import Depends
 from wapang.app.store.dto.responses import StoreDetailResponse
-from wapang.app.store.errors import StoreNotFoundError
+from wapang.app.store.errors import (
+    AlreadyHasStoreError,
+    StoreAlreadyExistsError,
+    StoreNotFoundError,
+)
 from wapang.app.store.store import StoreStore
 from wapang.app.user.models import User
 from wapang.common.errors import MissingRequiredFieldError
@@ -25,17 +29,15 @@ class StoreService:
         phone_number = phone_number or user.phone_number
         if not name or not address or not email or not phone_number:
             raise MissingRequiredFieldError()
-        try:
-            store = self.store_store.create_store(
-                name=name,
-                address=address,
-                email=email,
-                phone_number=phone_number,
-                owner_id=user.id,
-            )
-        except Exception as e:
-            print(e)
-            raise e
+
+        self.raise_if_store_exists(user.id, name, email, phone_number)
+        store = self.store_store.create_store(
+            name=name,
+            address=address,
+            email=email,
+            phone_number=phone_number,
+            owner_id=user.id,
+        )
         return StoreDetailResponse.model_validate(store, from_attributes=True)
 
     def get_store_by_id(self, store_id: int) -> StoreDetailResponse:
@@ -43,3 +45,17 @@ class StoreService:
         if store is None:
             raise StoreNotFoundError()
         return StoreDetailResponse.model_validate(store, from_attributes=True)
+
+    def raise_if_store_exists(
+        self, owner_id: int, name: str, email: str, phone_number: str
+    ) -> None:
+        store = self.store_store.get_store(owner_id, name, email, phone_number)
+        if store is not None:
+            if store.owner_id == owner_id:
+                raise AlreadyHasStoreError()
+            if store.name == name:
+                raise StoreAlreadyExistsError("Store")
+            if store.email == email:
+                raise StoreAlreadyExistsError("Email")
+            if store.phone_number == phone_number:
+                raise StoreAlreadyExistsError("Phone number")
--- wapang/app/store/store.py
+++ wapang/app/store/store.py
@@ -17,8 +17,6 @@ class StoreStore:
     def create_store(
         self, name: str, address: str, email: str, phone_number: str, owner_id: int
     ) -> Store:
-        self.check_already_exists(owner_id, name, email, phone_number)
-
         store = Store(
             name=name,
             address=address,
@@ -29,38 +27,29 @@ class StoreStore:
         self.session.add(store)
         self.session.flush()
         return store
-    
+
     def get_store_by_id(self, store_id: int) -> Store | None:
         get_store_query = select(Store).filter(Store.id == store_id)
         store = self.session.scalar(get_store_query)
         return store
-    
+
     def get_store_of_user(self, user_id: int) -> Store | None:
         get_store_query = select(Store).filter(Store.owner_id == user_id)
         store = self.session.scalar(get_store_query)
         return store
-    
+
     def get_store_by_name(self, name: str) -> Store | None:
         get_store_query = select(Store).filter(Store.name == name)
         store = self.session.scalar(get_store_query)
         return store
 
-    def check_already_exists(
+    def get_store(
         self, owner_id: int, name: str, email: str, phone_number: str
-    ) -> None:
-        store_already_exists_query = select(Store).filter(
+    ) -> Store | None:
+        get_store_query = select(Store).filter(
             (Store.owner_id == owner_id)
             | (Store.name == name)
             | (Store.email == email)
             | (Store.phone_number == phone_number)
         )
-        store_already_exists = self.session.scalar(store_already_exists_query)
-        if store_already_exists:
-            if store_already_exists.owner_id == owner_id:
-                raise AlreadyHasStoreError()
-            if store_already_exists.name == name:
-                raise StoreAlreadyExistsError("Store")
-            if store_already_exists.email == email:
-                raise StoreAlreadyExistsError("Email")
-            if store_already_exists.phone_number == phone_number:
-                raise StoreAlreadyExistsError("Phone number")
+        return self.session.scalar(get_store_query)
