3c8ca3ec7623ea3e9c5d65c4899e0c747a3e8ab5
Minkyu Lee <whizkyu@snu.ac.kr>
2024-12-07 13:46:06 +0900

    feat: make default session read only

--- wapang/database/middleware.py
+++ wapang/database/middleware.py
@@ -1,3 +1,4 @@
+from sqlalchemy import text
 from starlette.middleware.base import BaseHTTPMiddleware
 
 from wapang.database.connection import SESSION, reset_session, start_default_session
@@ -6,12 +7,12 @@ from wapang.database.connection import SESSION, reset_session, start_default_ses
 class DefaultSessionMiddleware(BaseHTTPMiddleware):
     async def dispatch(self, request, call_next):
         tokens = start_default_session()
+        await SESSION.execute(text("START TRANSACTION READ ONLY"))
         try:
             response = await call_next(request)
+            await SESSION.commit()
         except Exception as e:
             raise e
-        else:
-            await SESSION.commit()
         finally:
             await SESSION.close()
             reset_session(tokens)
